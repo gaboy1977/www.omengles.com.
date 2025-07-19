4560cdc003bf7cc127991909ec53a026d6175406
gaboy1977 <81317643+gaboy1977@users.noreply.github.com>
2025-07-19 14:23:29 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Adding an event listener to the "Start" button
+document.querySelector('.start-btn').addEventListener('click', function() {
+    alert('You clicked the Start button!');
+    // Add your logic here
+});
